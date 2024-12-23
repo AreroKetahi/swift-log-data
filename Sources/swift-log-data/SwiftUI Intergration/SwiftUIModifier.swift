71005f1881a8457422dd6725d3ee539974efcd2e
Arkivili Collindort <isoblack1@outlook.com>
2024-12-23 08:40:57 +0800

    Update SwiftUIModifier.swift

--- Sources/swift-log-data/SwiftUI Intergration/SwiftUIModifier.swift
+++ Sources/swift-log-data/SwiftUI Intergration/SwiftUIModifier.swift
@@ -19,3 +19,9 @@ extension View {
         self.modifier(LoggingModelContainerModifier())
     }
 }
+
+extension Scene {
+    public func logContainer() -> some Scene {
+        self.modelContainer(LogContainer.default.container)
+    }
+}
